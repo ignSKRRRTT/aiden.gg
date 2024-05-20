891a96ccadca05bc66c55774dc1f48d163e47023
xpert <90223556+xpertlol@users.noreply.github.com>
2024-05-20 01:37:08 -0600

    dfsf

---
+++ src/components/index/Hero.tsx
@@ -0,0 +1,19 @@
+export default function Hero({ inView, descRef }: { inView: boolean, descRef: any }) {
+  return (
+    <>
+      <section className="max-w-4xl w-full flex flex-col mx-auto pt-44">
+        <div
+          className="relative sm:p-12 p-6 w-full flex flex-col bg-gradient-to-br from-primary to to-secondary rounded-lg border-1 border-accent shadow-2xl shadow-background"
+          ref={descRef}
+        >
+          <h1 className="font-leaguespartan font-bold lg:text-7xl md:text-6xl sm:text-5xl text-4xl text-text">
+            Hey, I&apos;m Q<span className="sm:hidden inline-block animate-wave ml-2">👋</span><span className="font-leaguespartan font-bold text-xl text-text brightness-50 italic sm:inline flex"> aka Xpert</span><span className="sm:inline-block hidden animate-wave ml-2">👋</span>
+          </h1>
+          <p className="font-leaguespartan font-semibold lg:text-3xl md:text-2xl sm:text-xl text-lg text-text">
+            {inView && "I'm a 404 year old new developer located in New Mexico "}
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
