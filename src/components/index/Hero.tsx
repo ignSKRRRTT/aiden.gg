ef9b32ea61e6f934488480d75b1734b436985edd
xpert <90223556+xpertlol@users.noreply.github.com>
2024-05-20 01:38:14 -0600

    sadasd

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -7,10 +7,10 @@ export default function Hero({ inView, descRef }: { inView: boolean, descRef: an
           ref={descRef}
         >
           <h1 className="font-leaguespartan font-bold lg:text-7xl md:text-6xl sm:text-5xl text-4xl text-text">
-            Hey, I&apos;m Q<span className="sm:hidden inline-block animate-wave ml-2">👋</span><span className="font-leaguespartan font-bold text-xl text-text brightness-50 italic sm:inline flex"> aka Xpert</span><span className="sm:inline-block hidden animate-wave ml-2">👋</span>
+            Hey, I&apos;m Aiden<span className="sm:hidden inline-block animate-wave ml-2">👋</span><span className="font-leaguespartan font-bold text-xl text-text brightness-50 italic sm:inline flex"> aka Xpert</span><span className="sm:inline-block hidden animate-wave ml-2">👋</span>
           </h1>
           <p className="font-leaguespartan font-semibold lg:text-3xl md:text-2xl sm:text-xl text-lg text-text">
-            {inView && "I'm a 404 year old new developer located in New Mexico "}
+            {inView && "I'm a 22 year old cup maker located in Texas "}
           </p>
         </div>
       </section>
