b1aa2ab6bd46f7a44b4b31f74114a6fa510ca120
Aiden <80472468+ignSKRRRTT@users.noreply.github.com>
2024-05-27 02:12:02 -0400

    New tech, slight css changes, updated packages

--- src/components/TechBadge.tsx
+++ src/components/TechBadge.tsx
@@ -5,7 +5,7 @@ export default function TechBadge({ title, icon }:{ title: string, icon: ReactNo
     <>
       <li className="p-2 bg-secondary border-1 border-accent rounded-lg flex flex-row gap-2 items-center ml-2">
         {icon}
-        <span className="font-leaguespartan text-center font-semibold text-base text-text">{title}</span>
+        <span className="font-leaguespartan text-center font-semibold text-base text-text mt-0.5">{title}</span>
       </li>
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link href="https://unpkg.com/pattern.css@1.0.0/dist/pattern.min.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
